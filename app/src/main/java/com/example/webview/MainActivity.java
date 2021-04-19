f6b3b5c0f7e78b402adf580dc2e6b6d1c16b7949
yunjedi <kingofdragonjacklong2009@gmail.com>
2021-04-19 13:55:43 +0700

    int http version

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,86 +1,191 @@
 package com.example.webview;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-
+import android.webkit.URLUtil;
 import android.webkit.WebView;
-
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-//import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import com.example.webview.R;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class MainActivity extends Activity  {
+public class MainActivity extends AppCompatActivity {
 
-
-    private  EditText addressBar;
-
-    private WebView webView;
-    private Button buttonGo;
-    private Button buttonStatic;
-
-    //SwipeRefreshLayout swipe;
-
+    // Debug log tag.
+    private static final String TAG_HTTP_URL_CONNECTION = "HTTP_URL_CONNECTION";
+    // Child thread sent message type value to activity main thread Handler.
+    private static final int REQUEST_CODE_SHOW_RESPONSE_TEXT = 1;
+    // The key of message stored server returned data.
+    private static final String KEY_RESPONSE_TEXT = "KEY_RESPONSE_TEXT";
+    // Request method GET. The value must be uppercase.
+    private static final String REQUEST_METHOD_GET = "GET";
+    // Request web page url input text box.
+    private EditText requestUrlEditor = null;
+    // Send http request button.
+    private Button requestUrlButton = null;
+    // TextView to display server returned page html text.
+    private TextView responseTextView = null;
+    // This handler used to listen to child thread show return page html text message and display those text in responseTextView.
+    private Handler uiUpdater = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonGo =(Button)findViewById(R.id.button_go);
-        buttonStatic =(Button)findViewById(R.id.button_static);
-
-        addressBar =(EditText)findViewById(R.id.editText_addressBar);
-        webView =(WebView)findViewById(R.id.webView);
-
-
-        // Tùy biến WebViewClient để điều khiển các sự kiện trên WebView
-        webView.setWebViewClient(new MyWebViewClient(addressBar));
-
-
-//        swipe = (SwipeRefreshLayout)findViewById(R.id.swipe);
-//        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                goUrl();
-//            }
-//        });
-
-        buttonGo.setOnClickListener(new Button.OnClickListener() {
+        setTitle("HttpURLConnection");
+        // Init app ui controls.
+        initControls();
+        // When click request url button.
+        requestUrlButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                goUrl();
-            }
-        });
-
-        buttonStatic.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showStaticContent();
+            public void onClick(View v) {
+                String reqUrl =  requestUrlEditor.getText().toString();
+                if(!TextUtils.isEmpty(reqUrl))
+                {
+                    if(URLUtil.isHttpUrl(reqUrl) || URLUtil.isHttpsUrl(reqUrl))
+                    {
+                        startSendHttpRequestThread(reqUrl);
+                    }else
+                    {
+                        Toast.makeText(getApplicationContext(), "The request url is not a valid http or https url.", Toast.LENGTH_LONG).show();
+                    }
+                }else
+                {
+                    Toast.makeText(getApplicationContext(), "The request url can not be empty.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
-
-    private void goUrl()  {
-        String url = addressBar.getText().toString().trim();
-        if(url.isEmpty())  {
-            Toast.makeText(this,"Please enter url",Toast.LENGTH_SHORT).show();
-            return;
+    // Initialize app controls.
+    private void initControls()
+    {
+        if(requestUrlEditor == null)
+        {
+            requestUrlEditor = (EditText)findViewById(R.id.http_url_editor);
         }
-        webView.getSettings().setLoadsImagesAutomatically(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.loadUrl(url);
-        //swipe.setRefreshing(true);
+        if(requestUrlButton == null)
+        {
+            requestUrlButton = (Button)findViewById(R.id.http_url_request_button);
+        }
+        if(responseTextView == null)
+        {
+            responseTextView = (TextView) findViewById(R.id.http_url_response_text_view);
+        }
+        // This handler is used to wait for child thread message to update server response text in TextView.
+        if(uiUpdater == null)
+        {
+            uiUpdater = new Handler()
+            {
+                @Override
+                public void handleMessage(Message msg) {
+                    if(msg.what == REQUEST_CODE_SHOW_RESPONSE_TEXT)
+                    {
+                        Bundle bundle = msg.getData();
+                        if(bundle != null)
+                        {
+                            String responseText = bundle.getString(KEY_RESPONSE_TEXT);
+                            responseTextView.setText(responseText);
+                        }
+                    }
+                }
+            };
+        }
     }
-
-
-    private void showStaticContent()  {
-        String staticContent="<h2>Select web page</h2>"
-                + "<ul><li><a href='http://eclipse.org'>Eclipse</a></li>"
-                +"<li><a href='http://google.com'>Google</a></li></ul>";
-        webView.loadData(staticContent, "text/html", "UTF-8");
+    /* Start a thread to send http request to web server use HttpURLConnection object. */
+    private void startSendHttpRequestThread(final String reqUrl)
+    {
+        Thread sendHttpRequestThread = new Thread()
+        {
+            @Override
+            public void run() {
+                // Maintain http url connection.
+                HttpURLConnection httpConn = null;
+                // Read text input stream.
+                InputStreamReader isReader = null;
+                // Read text into buffer.
+                BufferedReader bufReader = null;
+                // Save server response text.
+                StringBuffer readTextBuf = new StringBuffer();
+                try {
+                    // Create a URL object use page url.
+                    URL url = new URL(reqUrl);
+                    // Open http connection to web server.
+                    httpConn = (HttpURLConnection)url.openConnection();
+                    // Set http request method to get.
+                    httpConn.setRequestMethod(REQUEST_METHOD_GET);
+                    // Set connection timeout and read timeout value.
+                    httpConn.setConnectTimeout(10000);
+                    httpConn.setReadTimeout(10000);
+                    // Get input stream from web url connection.
+                    InputStream inputStream = httpConn.getInputStream();
+                    // Create input stream reader based on url connection input stream.
+                    isReader = new InputStreamReader(inputStream);
+                    // Create buffered reader.
+                    bufReader = new BufferedReader(isReader);
+                    // Read line of text from server response.
+                    String line = bufReader.readLine();
+                    // Loop while return line is not null.
+                    while(line != null)
+                    {
+                        // Append the text to string buffer.
+                        readTextBuf.append(line);
+                        // Continue to read text line.
+                        line = bufReader.readLine();
+                    }
+                    // Send message to main thread to update response text in TextView after read all.
+                    Message message = new Message();
+                    // Set message type.
+                    message.what = REQUEST_CODE_SHOW_RESPONSE_TEXT;
+                    // Create a bundle object.
+                    Bundle bundle = new Bundle();
+                    // Put response text in the bundle with the special key.
+                    bundle.putString(KEY_RESPONSE_TEXT, readTextBuf.toString());
+                    // Set bundle data in message.
+                    message.setData(bundle);
+                    // Send message to main thread Handler to process.
+                    uiUpdater.sendMessage(message);
+                }catch(MalformedURLException ex)
+                {
+                    Log.e(TAG_HTTP_URL_CONNECTION, ex.getMessage(), ex);
+                }catch(IOException ex)
+                {
+                    Log.e(TAG_HTTP_URL_CONNECTION, ex.getMessage(), ex);
+                }finally {
+                    try {
+                        if (bufReader != null) {
+                            bufReader.close();
+                            bufReader = null;
+                        }
+                        if (isReader != null) {
+                            isReader.close();
+                            isReader = null;
+                        }
+                        if (httpConn != null) {
+                            httpConn.disconnect();
+                            httpConn = null;
+                        }
+                    }catch (IOException ex)
+                    {
+                        Log.e(TAG_HTTP_URL_CONNECTION, ex.getMessage(), ex);
+                    }
+                }
+            }
+        };
+        // Start the child thread to request web page.
+        sendHttpRequestThread.start();
     }
-
 }
